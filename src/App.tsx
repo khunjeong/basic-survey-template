cec91c0cb0b4451c4c071af7774adb1780681dcc
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-06 16:38:48 +0900

    Feat: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import type { FC } from 'react';
 import { ConfigProvider } from 'antd';
 import 'antd/dist/reset.css';
+import '@styles/global.css';
 import { Button } from '@components';
+import { Reset } from 'styled-reset';
 
 const App: FC = () => (
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#00b96b',
-      },
-    }}>
-    <Button>Button</Button>
-  </ConfigProvider>
+  <div className='App'>
+    <Reset />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00b96b',
+        },
+      }}>
+      <Button>Button</Button>
+    </ConfigProvider>
+  </div>
 );
 
 export default App;
---
+++ src/styles/colors.js
@@ -0,0 +1,52 @@
+module.exports = {
+  PRIMARY: '#45D6C8',
+  PRIMARY_BRIGHT: '#97EEE6',
+  PRIMARY_LIGHT: '#EBFFFD', // Hovered
+  PRIMARY_DARK: '#3AABA0', // Clicked
+
+  // Sencondary
+  RED_SOFT: '#fff9f9',
+  RED_LIGHT: '#fae2e2', // Message box
+  RED_BRIGHT: '#f07799', // InputHover/InputCaret
+  RED_ORIGIN: '#f60303', // Error
+
+  // Others
+  BLUE_SOFT: '#f3fbff',
+  BLUE_LIGHT: '#d4f0ff',
+  BLUE_BRIGHT: '#6ccaff',
+  BLUE_ORIGIN: '#00a3ff',
+  BLUE_DARK: '#0082cc',
+  GREEN_LIGHT: '#DCFFE4',
+  GREEN_ORIGIN: '#00E033',
+  GREEN_DARK: '#02C92F',
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+
+  // Gray Light
+  GRAY_LIGHT_1: '#fafafa', // BG/Text/InputBG
+  GRAY_LIGHT_2: '#f9f9f9', // CardBG
+  GRAY_LIGHT_3: '#f5f5f5', // Border/Default
+  GRAY_LIGHT_4: '#f0f0f0', // InputDisabledBG/CardBG
+  GRAY_LIGHT_5: '#ebebeb', // InputBorder
+
+  // Gray Bright
+  GRAY_BRIGHT_1: '#c4c4c4', // Text/Disabled/Placeholder
+  GRAY_BRIGHT_2: '#cccccc', // Icon/Border
+  GRAY_BRIGHT_3: '#aaaaaa', // Icon/Border
+
+  // Gray Origin
+  GRAY_ORIGIN_1: '#999999',
+  GRAY_ORIGIN_2: '#878787', // Text/Bolder
+  GRAY_ORIGIN_3: '#777777',
+
+  // Gray Strong
+  GRAY_STRONG_1: '#666666',
+  GRAY_STRONG_2: '#555555',
+  GRAY_STRONG_3: '#444444',
+
+  // Gray Dark
+  GRAY_DARK: '#2f2f2f', // Text/Default/Icon
+  GRAY_DARK_2: '#202020',
+
+  BOX_SHADOW: 'rgba(0, 0, 0, 0.24)', // BoxShadow Color
+};
