dce391672c0177f1795c8cbc466910057f1ff668
khunjeong <51750678+khunjeong@users.noreply.github.com>
2023-03-09 21:32:54 +0900

    Feat: Text Component 추가

---
+++ src/components/Text/Text.styled.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { colors } from '../../styles/colors';
+import { ITextProps } from './Text.type';
+
+export const Title = styled.h2<ITextProps>`
+  font-size: ${props => props.size || '1.5em'};
+  font-weight: ${props => props.weight || 700};
+  color: ${props => props.color || colors.BLACK};
+  ${props =>
+    props.isEllipsis &&
+    `
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  `}
+`;
+
+export const Description = styled.p<ITextProps>`
+  font-size: ${props => props.size || '0.85em'};
+  font-weight: ${props => props.weight || 700};
+  color: ${props => props.color || colors.GRAY_ORIGIN_1};
+  ${props =>
+    props.isEllipsis &&
+    `
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  `}
+`;
+
+export const Text = styled.p<ITextProps>`
+  font-size: ${props => props.size || '1em'};
+  font-weight: ${props => props.weight || 700};
+  color: ${props => props.color || colors.BLACK};
+  ${props =>
+    props.isEllipsis &&
+    `
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    `}
+`;
